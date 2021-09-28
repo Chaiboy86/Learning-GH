550c98c0e693fb406b788330117f4adf6c11cad9
Kazmi Yamazato <kazmi.yamazato@gmail.com>
2021-09-28 19:08:11 +0900

    Hello Code Crew

--- Quote/Views/QuoteListView.swift
+++ Quote/Views/QuoteListView.swift
@@ -41,7 +41,7 @@ struct QuoteListView: View {
                     }
                 }
             }
-            .navigationTitle("Quotes")
+            .navigationTitle("Quotes List")
         }
     }
 }
